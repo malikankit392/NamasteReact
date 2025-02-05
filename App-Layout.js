62dc7ba05f03a2782c6c50c4cd39c0a5e987fbf9
Ankit Malik <malikankit392@gmail.com>
2025-02-05 17:24:05 +0530

    changes for when we fetched data from an obj in the functional component

--- App-Layout.js
+++ App-Layout.js
@@ -29,14 +29,97 @@ const Header = () => {
     )
 }
 
+const resObj= {
+    "info": {
+    "id": "722840",
+    "name": "Chinese Wok",
+    "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+    "locality": "Nagar Road",
+    "areaName": "Phoenix Marketcity Mall",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Chinese",
+    "Asian",
+    "Tibetan",
+    "Desserts"
+    ],
+    "avgRating": 4.3,
+    "parentId": "61955",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "592",
+    "sla": {
+    "deliveryTime": 29,
+    "lastMileTravel": 0.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "0.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-05 23:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹149"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/chinese-wok-nagar-road-phoenix-marketcity-mall-rest722840",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+
 const RestaurantCard = (props) => {
+    const { resData } = props;
+
     return (
         <div className= "res-card" style = {{backgroundColor: "#D3D3D3"}}>
-            <img className= "res-logo" alt-text="Meghna Foods" src="https://coreldrawdesign.com/resources/previews/preview-restaurants-logo-design-cdr-template-vector-free-1704712805.jpg"/>
-            <h3>{props.resName}</h3>
-            <h4>{props.cuisine}</h4>
-            <h4>{props.rating}</h4>
-            <h4>{props.eta}</h4>
+            <img className= "res-logo" alt-text="Meghna Foods" 
+                src = { "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + 
+                resData.info.cloudinaryImageId }/>
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.cuisines.join(", ")}</h4>
+            <h4>{resData.info.avgRating} stars</h4>
+            <h4>{resData.info.sla.slaString}</h4>
         </div>
     )
 }
@@ -46,8 +129,8 @@ const Body = () => {
         <div className ="body"> 
             <div className ="search">Search</div>
             <div className = "res-container">
-                <RestaurantCard resName="Meghna Foods" cuisine="Biryani, North Indian, Asian" rating="3.8" eta="30mins"/>
-                <RestaurantCard resName="KFC" cuisine="Burger and Fast food" rating="4.1" eta="40mins"/>
+                <RestaurantCard resData = {resObj}/>
+                
             </div> 
         </div>
     )
