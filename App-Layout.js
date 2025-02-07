994992c267fc9e6039a3010fb7ced4d9dab406ab
Ankit Malik <malikankit392@gmail.com>
2025-02-07 17:18:36 +0530

    episode 4 end, with best practices

--- App-Layout.js
+++ App-Layout.js
@@ -29,7 +29,80 @@ const Header = () => {
     )
 }
 
-const resObj= {
+const resList= [
+    {
+    "info": {
+    "id": "43909",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+    "locality": "Pheonix Market",
+    "areaName": "Viman Nagar",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Pizzas"
+    ],
+    "avgRating": 4.1,
+    "parentId": "721",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "2.9K+",
+    "sla": {
+    "deliveryTime": 31,
+    "lastMileTravel": 0.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "30-35 mins",
+    "lastMileTravelString": "0.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-05 23:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/pizza-hut-pheonix-market-viman-nagar-rest43909",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
     "info": {
     "id": "722840",
     "name": "Chinese Wok",
@@ -106,20 +179,605 @@ const resObj= {
     "type": "WEBLINK"
     },
     "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "5928",
+    "name": "Burger King",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/7f76a072-c1bc-4d74-ac56-33e0eea20c1e_5928.JPG",
+    "locality": "Phoenix Market city",
+    "areaName": "Viman Nagar",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Burgers",
+    "American"
+    ],
+    "avgRating": 4.1,
+    "parentId": "166",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "41K+",
+    "sla": {
+    "deliveryTime": 41,
+    "lastMileTravel": 0.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "40-45 mins",
+    "lastMileTravelString": "0.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-06 00:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
     }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/burger-king-phoenix-market-city-viman-nagar-rest5928",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "21743",
+    "name": "Chai Point",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/22/f2c67e8c-76b2-425d-a896-d7f26f425eb1_21743.jpg",
+    "locality": "Viman Nagar",
+    "areaName": "Viman Nagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "Bakery",
+    "Beverages",
+    "Maharashtrian",
+    "Snacks",
+    "Street Food",
+    "South Indian",
+    "Punjabi",
+    "Chaat",
+    "Indian",
+    "American"
+    ],
+    "avgRating": 4.4,
+    "parentId": "1607",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "7.6K+",
+    "sla": {
+    "deliveryTime": 32,
+    "lastMileTravel": 0.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "30-35 mins",
+    "lastMileTravelString": "0.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-05 23:00:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "50% OFF",
+    "subHeader": "UPTO ₹100"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "4.3",
+    "ratingCount": "377"
+    },
+    "source": "GOOGLE",
+    "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/chai-point-viman-nagar-rest21743",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "191348",
+    "name": "The Biryani Life",
+    "cloudinaryImageId": "z8gnc9uzws0q56hi2mcr",
+    "locality": "Rosary School",
+    "areaName": "Viman Nagar",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Biryani",
+    "Mughlai",
+    "Lucknowi",
+    "Hyderabadi",
+    "Kebabs",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.2,
+    "parentId": "8496",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "1.5K+",
+    "sla": {
+    "deliveryTime": 21,
+    "lastMileTravel": 0.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "0.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-05 23:59:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png",
+    "description": "bolt!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "bolt!",
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹49"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/the-biryani-life-rosary-school-viman-nagar-rest191348",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "32447",
+    "name": "Natural Ice Cream",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/3/ba4ce190-b1bf-4c29-997f-ff5eedb9b333_32447.JPG",
+    "locality": "Riddh Enclave",
+    "areaName": "Viman Nagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "Ice Cream",
+    "Desserts"
+    ],
+    "avgRating": 4.8,
+    "veg": true,
+    "parentId": "2093",
+    "avgRatingString": "4.8",
+    "totalRatingsString": "11K+",
+    "sla": {
+    "deliveryTime": 15,
+    "lastMileTravel": 1.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "10-15 mins",
+    "lastMileTravelString": "1.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-05 23:15:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png",
+    "description": "bolt!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "bolt!",
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "20% OFF",
+    "subHeader": "UPTO ₹50"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/natural-ice-cream-riddh-enclave-viman-nagar-rest32447",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "818595",
+    "name": "Olio - The Wood Fired Pizzeria",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/26/d894eb3d-b9f6-4a52-a14f-1a6a4cc91dfb_818595.jpg",
+    "locality": "Sangamwadi",
+    "areaName": "WADGAON SHERI",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Pizzas",
+    "Pastas",
+    "Italian",
+    "Fast Food",
+    "Snacks",
+    "Beverages",
+    "Desserts"
+    ],
+    "avgRating": 4.2,
+    "parentId": "11633",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "1.0K+",
+    "sla": {
+    "deliveryTime": 30,
+    "lastMileTravel": 1.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "30-35 mins",
+    "lastMileTravelString": "1.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-06 00:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png",
+    "description": "bolt!"
+    },
+    {
+    "imageId": "newg.png",
+    "description": "Gourmet"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "bolt!",
+    "imageId": "bolt/Bolt%20Listing%20badge@3x.png"
+    }
+    },
+    {
+    "attributes": {
+    "description": "Gourmet",
+    "imageId": "newg.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹99"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/olio-the-wood-fired-pizzeria-sangamwadi-wadgaon-sheri-rest818595",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "566278",
+    "name": "Faasos' Signature Wraps & Rolls",
+    "cloudinaryImageId": "c583ca6ce40b426797a78ae2ac91f2ec",
+    "locality": "Rosary School",
+    "areaName": "Viman Nagar",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Fast Food",
+    "Snacks",
+    "North Indian",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.3,
+    "parentId": "340366",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "135",
+    "sla": {
+    "deliveryTime": 21,
+    "lastMileTravel": 0.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "0.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-02-05 23:59:00",
+    "opened": true
+    },
+    "badges": {
+    "textExtendedBadges": [
+    {
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available",
+    "fontColor": "#7E808C"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "",
+    "fontColor": "#7E808C",
+    "iconId": "guiltfree/GF_Logo_android_3x",
+    "shortDescription": "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹63",
+    "discountCalloutInfo": {
+    "message": "Free Delivery",
+    "logoCtx": {
+    "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+    }
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-b0ba85d2-bcc5-484f-97ee-ac6eaddfe80f"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/pune/faasos-signature-wraps-and-rolls-rosary-school-viman-nagar-rest566278",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+    ]
 
 const RestaurantCard = (props) => {
     const { resData } = props;
+    const { cloudinaryImageId, name, cuisines, avgRating, sla} = resData?.info;
 
     return (
         <div className= "res-card" style = {{backgroundColor: "#D3D3D3"}}>
             <img className= "res-logo" alt-text="Meghna Foods" 
                 src = { "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + 
-                resData.info.cloudinaryImageId }/>
-            <h3>{resData.info.name}</h3>
-            <h4>{resData.info.cuisines.join(", ")}</h4>
-            <h4>{resData.info.avgRating} stars</h4>
-            <h4>{resData.info.sla.slaString}</h4>
+                cloudinaryImageId }/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{sla.slaString}</h4>
         </div>
     )
 }
@@ -129,8 +787,7 @@ const Body = () => {
         <div className ="body"> 
             <div className ="search">Search</div>
             <div className = "res-container">
-                <RestaurantCard resData = {resObj}/>
-                
+                { resList.map(rest => <RestaurantCard key = {rest.info.id} resData = {rest}/>)}
             </div> 
         </div>
     )
